ba9879fa2f6daf053dc252ada05d7cfa089962cf
Giovanni Schenato <gischenato@gmail.com>
2023-05-08 12:20:27 -0300

    removing unused imports

--- src/main/java/br/pucrs/engsoft2/banco/controllers/DisciplineController.java
+++ src/main/java/br/pucrs/engsoft2/banco/controllers/DisciplineController.java
@@ -2,12 +2,10 @@ package br.pucrs.engsoft2.banco.controllers;
 
 import java.util.List;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import br.pucrs.engsoft2.banco.beans.Discipline;
-import br.pucrs.engsoft2.banco.repositories.DisciplineRepository;
 import br.pucrs.engsoft2.banco.repositories.DisciplineService;
 import lombok.AllArgsConstructor;
 
