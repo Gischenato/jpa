1385deb73dfc237e696a30863a6125a029c2a50e
Giovanni Schenato <gischenato@gmail.com>
2023-05-23 11:44:23 -0300

    removing unused files

--- src/main/java/br/pucrs/engsoft2/banco/repositories/StudentRepository.java
+++
@@ -1,8 +0,0 @@
-package br.pucrs.engsoft2.banco.repositories;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import br.pucrs.engsoft2.banco.beans.Student;
-
-public interface StudentRepository extends JpaRepository<Student, Long>{
-}
